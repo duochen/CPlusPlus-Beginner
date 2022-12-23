a097be5f8e2b77b8e8bfef376e1eb2b5656a6966
Duo Chen <duochen@yahoo.com>
2022-12-22 23:12:40 -0600

    check in code

---
+++ Lesson01/Demo04_01/Demo04_01/Demo04_01.cpp
@@ -0,0 +1,12 @@
+// Demo01.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+int main()
+{
+    // this is a comment
+    // this is another comment
+    /*
+        This is a 
+        multi-line comment
+    */
+}
