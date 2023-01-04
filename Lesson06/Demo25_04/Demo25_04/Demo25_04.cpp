1e4b1605446ce40623e7c9ccd042d6957ad6e9e3
duochen <duochen@yahoo.com>
2023-01-04 17:32:02 -0600

    check in code

---
+++ Lesson06/Demo25_04/Demo25_04/Demo25_04.cpp
@@ -0,0 +1,32 @@
+// Demo25_04.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+class MyBaseClass
+{
+private:
+    char c;
+    int x;
+};
+
+class MyDerivedClass : public MyBaseClass
+{
+    // c and x NOT accessible here
+};
+
+int main()
+{
+    MyDerivedClass o;
+    o.c = 'a';          // Error, not accessible to object
+    o.x = 123;          // Error, not accessible to object
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
